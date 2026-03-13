e532c647b70d879c548e02155109b1bba6d04d88
Patrick Talty-Kerr <patricktaltykerr@outlook.com>
2026-03-13 17:58:47 +0000

    finished adding simple auth implementation with OAuth2 scheme using jwt

--- routes/auth.py
+++ routes/auth.py
@@ -1,8 +1,22 @@
+from datetime import timedelta
+
 from fastapi import APIRouter
 from database.session import get_db
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from sqlalchemy.orm import Session
-from services.authservice import create_user, does_user_exist, verify_password, is_password_strong
+from pydanticmodels.user import User
+from services.authservice import create_access_token, create_user, does_user_exist, get_current_active_user, get_password_hash, verify_password, is_password_strong, does_username_password_match
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from typing import Annotated
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+BASE_DIR = Path(__file__).resolve().parents[1]
+load_dotenv(BASE_DIR / ".env")
+TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 router = APIRouter(prefix="/auth")
 
@@ -10,21 +24,35 @@ router = APIRouter(prefix="/auth")
 async def register(username: str, password: str, db: Session = Depends(get_db)):
     if not is_password_strong(password):
         return {"error": "Password is not strong enough"}
-    if does_user_exist(username):
+    if does_user_exist(username, db):
         return {"error": "User already exists"}
     if create_user(username, password, db):
         return {"message": "User registered successfully"}
     return {"error": "Failed to register user"}
 
 @router.post("/login")
-async def login():
-    return {"todo": "Implement user login"}
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)):
+    user = form_data.username
+    if not does_user_exist(user, db):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    if not does_username_password_match(user, form_data.password, db):
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    print(BASE_DIR)
+    print(TOKEN_EXPIRE_MINUTES)
+    access_token_expires = timedelta(minutes=int(TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+        user, expires_delta=access_token_expires
+    )
+    
+    return {"access_token": access_token, "token_type": "bearer"}
 
 @router.post("/logout")
 async def logout():
-    return {"todo": "Implement user logout"}
+    return {"message": "Logged out. Delete token on client."} #frontend will delete token from headers
 
 @router.get("/me")
-async def get_current_user():
-    return {"todo": "Implement get current user"}
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+) -> User:
+    return current_user
 
--- services/authservice.py
+++ services/authservice.py
@@ -1,34 +1,107 @@
-from pwdlib import PasswordHash
-from sqlalchemy.orm import Session
+from datetime import datetime, timedelta, timezone
 
-from models.user import User
+from pwdlib import PasswordHash
+from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from database.session import get_db
+from pydanticmodels.user import User as UserSchema
+from models.user import User as UserModel 
+from typing import Annotated
+import jwt
+from jwt.exceptions import InvalidTokenError
+import os
+from dotenv import load_dotenv
+from fastapi import HTTPException, Depends, status
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parents[1]
+load_dotenv(BASE_DIR / ".env")
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
 
 
 passwordHash = PasswordHash.recommended()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
 
-##pass word fucntions
+##pass helper word fucntions
 def get_password_hash(plainPassword: str):
     return passwordHash.hash(plainPassword)
 
 def verify_password(plainPassword: str, hashedPassword: str) -> bool:
     return passwordHash.verify(plainPassword, hashedPassword)
 
+
 def is_password_strong(password: str) -> bool:
     return True
 
-##user fucntions
+##mew user sign up function
 def create_user(username: str, password: str, db: Session) -> bool:
-    if does_user_exist(username):
+    if does_user_exist(username, db):
         return False
     
     new_hashed_password = get_password_hash(password)
-    db.add(User(username=username, hashed_password=new_hashed_password))
+    db.add(UserModel(username=username, hashed_password=new_hashed_password))
     db.commit()
     return True
 
-def does_user_exist(username: str) -> bool:
-    return False
+
+##existing user login
+def does_username_password_match(username: str, password: str, db: Session) -> bool:
+    user = db.query(UserModel).filter(UserModel.username == username).first()
+    if not user:
+        return False
+    return verify_password(password, user.hashed_password)
+
+##check if username exists in the database
+def does_user_exist(username: str, db: Session) -> bool:
+    q = db.query(UserModel.username).filter(UserModel.username == username)
+    return db.query(q.exists()).scalar()
+
+def retrieve_user(username: str, db: Session) -> UserSchema | None:
+    return db.query(UserModel).filter(UserModel.username == username).first() 
 
 
+def create_access_token(username: str, expires_delta: timedelta | None = None):
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=int(TOKEN_EXPIRE_MINUTES)))
+    to_encode = {"sub": username, "exp": expire}
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
+    
+## current logged in user    
+async def get_current_active_user(
+    current_user: Annotated[UserSchema, Depends(get_current_user_normal)],
+    db: Session = Depends(get_db)
+):
+    return current_user
+
+
+async def get_current_user_normal(token: Annotated[str, Depends(oauth2_scheme)], db: Session = Depends(get_db)) -> UserSchema:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except InvalidTokenError:
+        raise credentials_exception
+    user = retrieve_user(token_data.username, db)
+    if user is None:
+        raise credentials_exception
+    return user
 
